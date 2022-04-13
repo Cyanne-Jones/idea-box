f9f864d59e0b3e813ccdbac5a47bbc9c84bd14c3
Catlyn Bowles <bowlescatlyn@gmail.com>
2022-04-13 11:19:34 -0600

    Add show idea function

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 // QUERY SELECTORS
 var userTitle = document.querySelector('#title-input');
 var userBody = document.querySelector('#body-input');
+var ideaDisplay = document.querySelector('.idea-display')
 
 var savedButton = document.querySelector('.saved-button');
 
 // EVENT LISTENERS
-savedButton.addEventListener('click', saveIdea);
+savedButton.addEventListener('click', showIdea);
 
 // ARRAY
 var ideas = [];
@@ -16,4 +17,25 @@ function saveIdea(event) {
   var newIdea = new Idea (userTitle.value, userBody.value);
   ideas.push(newIdea);
   console.log(ideas)
+}
+
+function showIdea() {
+  saveIdea(event);
+  ideaDisplay.innerHTML = '';
+  for (var i = 0; i < ideas.length; i++) {
+    ideaDisplay.innerHTML += `<article class='idea-box'>
+      <section class='top-bar'>
+        <img src='./assets/star-active.svg' alt='orange star'>
+        <img src='./assets/delete.svg' alt='white x'>
+      </section>
+      <section class='box-body'>
+        <h3 class='idea-title'>${ideas[i].title}</h3>
+        <p class='idea-body'>${ideas[i].body}</p>
+      </section>
+      <section class='bottom-bar'>
+        <img src='./assets/comment.svg' alt='plus sign'>
+        <h4 class='idea-box-comment'>Comment</h4>
+      </section>
+    </article>`
+  }
 }
