106940a23d17fc176e53e0d2c8eb75cb2f9f578b
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-04-14 11:08:22 -0600

    Fix save button bugs

--- main.js
+++ main.js
@@ -8,17 +8,21 @@ var savedButton = document.querySelector('.saved-button');
 
 // EVENT LISTENERS
 savedButton.addEventListener('click', showIdea);
-inputBoxes.addEventListener('change', enableButton);
+// if we wanted the query selector to be input class we would have had to create a for loop for it to work since it's a collection of elements.
+inputBoxes.addEventListener('input', enableButton);
+// using input instead of change solves our issue with needing to click outside of the input boxes to make the save button change color and enableButton
 
 function disableButton() {
-  savedButton.disable = true;
+  savedButton.disabled = true;
+  // need to change from disable to disabled
   savedButton.classList.remove('save-en');
   savedButton.classList.add('save-dis');
 }
 
 function enableButton() {
   if (userTitle.value && userBody.value) {
-    savedButton.disable = false;
+    savedButton.disabled = false;
+    // need to change from disable to disabled
     savedButton.classList.remove('save-dis');
     savedButton.classList.add('save-en');
   } else {
@@ -36,6 +40,8 @@ function saveIdea(event) {
   ideas.push(newIdea);
   userTitle.value = '';
   userBody.value = '';
+  disableButton();
+  // had to add disableButton function here so that when you click back into the form after the first submission it doesn't disable the button.
 }
 
 function showIdea() {
